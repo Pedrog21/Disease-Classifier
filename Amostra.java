cf377516ddb9309a45268e579bdc23103984b6ae
Pedro Gonçalves <pedrogoncalves@Air-de-Pedro.home>
2018-03-24 16:23:32 +0000

    Alterei a classe Amostra

--- Amostra.java
+++ Amostra.java
@@ -4,6 +4,8 @@ import java.util.Vector;
 
 public class Amostra {
 	private Vector<Vector<Integer>> vec;
+	private int length;
+	
 
 	public Vector<Vector<Integer>> getVec() {
 		return vec;
@@ -13,15 +15,33 @@ public class Amostra {
 		this.vec = vec;
 	}
 	
+	
+	public int getLength() {
+		return length;
+	}
+
+	public void setLength(int length) {
+		this.length = length;
+	}
+	
+	
+
 	public void add(Vector<Integer> v) {
 		vec.add(v);
 		
 	}
 	
 	public int length() {
-		return vec.size();
+		return length;
+	}
+	
+	public Vector<Integer> element(int k){
+		return vec.get(k);
 		
 	}
+	
+	
+	
 	
 
 }
