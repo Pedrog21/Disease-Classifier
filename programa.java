59ccf5bb853e9f76bd4227aa7e56fddbc6a7179e
miguelnascimento <37712149+miguelnascimento@users.noreply.github.com>
2018-04-20 12:27:25 +0100

    Update programa.java

--- programa.java
+++ programa.java
@@ -12,24 +12,23 @@ public class programa {
 		amostra.add(c);
 		List<Integer> q= Arrays.asList(1,2,6,3);
 		amostra.add(q);
-		Node f = new Node(2);
+		
+		
+		Node f = new Node(4);
 		Node t = new Node(1);
-		ArrayList<Edges> r = new ArrayList<Edges>();
-		Edges w = new Edges(t,f, 4);
-		r.add(w);
-		
-
-		WGraph o = new WGraph(2);
+		WGraph o = new WGraph(4);
 		o.add_edge(t,f,2);
+		o.add_edge(t,new Node(2),5);
+		o.add_edge(new Node(3),new Node(2),7);
+		o.add_edge(new Node(5),new Node(2),9);
+		o.add_edge(new Node(1),new Node(2),8);
 		o.remove_edge(t,f);
-		
-		
+		o.remove_edge(new Node(2),f);
 		System.out.println(o);
 		
 
      
 		System.out.println(amostra.getT());
-		
 		
 		List<Integer> a= Arrays.asList(0,1);
 		List<Integer> b= Arrays.asList(1,4);
