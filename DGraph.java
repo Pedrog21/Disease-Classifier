c499e3d54727abac9ee54395a3f48a2b4de72edc
JoaoMiguelNC <38663724+JoaoMiguelNC@users.noreply.github.com>
2018-04-26 14:44:43 +0100

    Add files via upload

---
+++ DGraph.java
@@ -0,0 +1,46 @@
+package Classes;
+
+import java.util.ArrayList;
+
+public class DGraph {
+	
+	private int dim;
+	private ArrayList<ArrayList<Integer>> edges;
+	
+	public DGraph(int n) {
+		dim = n;
+		edges = new ArrayList<ArrayList<Integer>>();
+		for (int i = 0; i < n; i++) {
+			edges.add(new ArrayList<Integer>());
+		}
+	}
+	
+	public void add_edge(int u, int v) {
+		if (!edges.get(u).contains(v)) {
+			edges.get(u).add(v);
+		}
+	}
+	
+	public void remove_edge(int u, int v) {
+		int i = edges.get(u).indexOf(v); 
+		if (i != -1) edges.get(u).remove(i);
+	}
+	
+	public ArrayList<Integer> parents(int v) {
+		ArrayList<Integer> r = new ArrayList<Integer>();
+		for (int i = 0; i < dim; i++) {
+			if (edges.get(i).contains(v)) r.add(i);
+		}
+		return r;
+	}
+	
+	public int get_dim() {
+		return dim;
+	}
+	
+	@Override
+	public String toString() {
+		return edges.toString();
+	}
+
+}
